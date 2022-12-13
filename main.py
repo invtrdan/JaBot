54530545882d0ac2647a2f61067b9dfd6e5ba5c4
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2022-12-13 07:01:24 +0000

    InProgress, NewOrder routes and corresponding html files.

--- main.py
+++ main.py
@@ -11,4 +11,12 @@ def home():
 def orders():
     return render_template('orders.html')
 
+@app.route('/InProgress')
+def orders_in_progress():
+  return render_template('ordes_in_progress.html')
+
+@app.route('/NewOrder')
+def new_order():
+  return render_template('new_order.html')
+
 app.run(host='0.0.0.0', port=81)
