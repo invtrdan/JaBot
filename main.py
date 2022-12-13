82fe18ee3b2cd023165c13c8c9e9da9bd5a5880c
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2022-12-13 01:24:46 -0500

    added '/' (home) and '/orders' routes

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from flask import Flask, render_template, request, jsonify
+import json
+
+app = Flask('app')
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/orders')
+def orders():
+    return render_template('orders.html')
+
+app.run(host='0.0.0.0', port=81)
